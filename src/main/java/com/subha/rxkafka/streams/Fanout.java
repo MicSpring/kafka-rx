28f5bfcef23ee4f10590ec98cd38eb8c44cf41a5
micspring <subhafromslg123@gmail.com>
2017-05-09 00:43:14 +0530

    Emitter Processor Inception

---
+++ src/main/java/com/subha/rxkafka/streams/Fanout.java
@@ -0,0 +1,34 @@
+package com.subha.rxkafka.streams;
+
+import reactor.core.publisher.BlockingSink;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+
+/**
+ * Created by user on 5/9/2017.
+ */
+public class Fanout {
+    public static void main(String[] args){
+
+       EmitterProcessor<String> emitterProcessor = EmitterProcessor.create();
+        BlockingSink<String> blockingSink = emitterProcessor.connectSink();
+
+        emitterProcessor.subscribe(s -> System.out.println("Fine for:"+s));
+        emitterProcessor.onNext("i AM fINEEE");
+
+        emitterProcessor.subscribe(s -> System.out.println(s));
+
+       Flux<BlockingSink.Emission> blockingSinkFlux =
+               Flux.<Integer>range(1,3).map(integer -> blockingSink.emit(String.valueOf(integer)));
+
+      /* Flux<String> stringFlux = emitterProcessor.publishOn(Schedulers.fromExecutor(Executors.newSingleThreadExecutor()))
+               .doOnNext(s -> System.out.println("Data is:"+s))
+               .map(s -> {
+           System.out.println("The Data is:"+s);
+           return s+"Processed";});*/
+
+        blockingSinkFlux.subscribe(s -> System.out.println(s));
+
+
+    }
+}
